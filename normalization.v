8b0b87294b69caa85c5616cbbb7dfded8cec0f52
Zahid <zahidx27@gmail.com>
2025-09-26 17:06:14 +0530

    Update normalization.v

--- normalization.v
+++ normalization.v
@@ -1,19 +1,23 @@
-`timescale 1ns / 1ps (* KEEP_HIERARCHY = "TRUE", DONT_TOUCH = "TRUE" *)
-
 module normalization (
-    input [16:0] mantissa_i,
+    input [10:0] mantissa_i,
     input [9:0] exponent_i,
     output reg [9:0] exponent,
-    output reg [16:0] mantissa
+    output [7:0] mantissa_pd
 );
+  reg overflow;
+  reg [10:0] mantissa;
   always @* begin
-    if (mantissa_i[16] == 1'b1) begin
+    if (mantissa_i[10] == 1'b1) begin
       exponent = exponent_i + 1'd1;
+      overflow = 1;
       // logical right shift
       mantissa = mantissa_i >> 1;
     end else begin
       exponent = exponent_i;
       mantissa = mantissa_i;
+      overflow = 0;
     end
   end
+
+  assign mantissa_pd = mantissa[9:2];
 endmodule
