cd7573162879e46a0a74e7fa5eb22538c8f4c614
Zahid <zahidx27@gmail.com>
2025-09-26 17:03:14 +0530

    Update carry_propogate_adder.v

--- carry_propogate_adder.v
+++ carry_propogate_adder.v
@@ -1,130 +1,86 @@
-`timescale 1ns / 1ps
-
 module carry_prop_adder (
-    input  [16:0] in1,
-    input  [16:0] in2,
-    output [16:0] sum
+    input  [10:0] in1,
+    input  [10:0] in2,
+    output [10:0] sum
 );
-  wire [17:0] in3;
-  assign in3[0] = 0;
+  wire [11:0] carry;
+  assign carry[0] = 0;
   fullAdder fa0 (
       .a(in1[0]),
       .b(in2[0]),
-      .c(in3[0]),
+      .c(carry[0]),
       .sum(sum[0]),
-      .cout(in3[1])
+      .cout(carry[1])
   );
   fullAdder fa1 (
       .a(in1[1]),
       .b(in2[1]),
-      .c(in3[1]),
+      .c(carry[1]),
       .sum(sum[1]),
-      .cout(in3[2])
+      .cout(carry[2])
   );
   fullAdder fa2 (
       .a(in1[2]),
       .b(in2[2]),
-      .c(in3[2]),
+      .c(carry[2]),
       .sum(sum[2]),
-      .cout(in3[3])
+      .cout(carry[3])
   );
   fullAdder fa3 (
       .a(in1[3]),
       .b(in2[3]),
-      .c(in3[3]),
+      .c(carry[3]),
       .sum(sum[3]),
-      .cout(in3[4])
+      .cout(carry[4])
   );
   fullAdder fa4 (
       .a(in1[4]),
       .b(in2[4]),
-      .c(in3[4]),
+      .c(carry[4]),
       .sum(sum[4]),
-      .cout(in3[5])
+      .cout(carry[5])
   );
   fullAdder fa5 (
       .a(in1[5]),
       .b(in2[5]),
-      .c(in3[5]),
+      .c(carry[5]),
       .sum(sum[5]),
-      .cout(in3[6])
+      .cout(carry[6])
   );
   fullAdder fa6 (
       .a(in1[6]),
       .b(in2[6]),
-      .c(in3[6]),
+      .c(carry[6]),
       .sum(sum[6]),
-      .cout(in3[7])
+      .cout(carry[7])
   );
   fullAdder fa7 (
       .a(in1[7]),
       .b(in2[7]),
-      .c(in3[7]),
+      .c(carry[7]),
       .sum(sum[7]),
-      .cout(in3[8])
+      .cout(carry[8])
   );
   fullAdder fa8 (
       .a(in1[8]),
       .b(in2[8]),
-      .c(in3[8]),
+      .c(carry[8]),
       .sum(sum[8]),
-      .cout(in3[9])
+      .cout(carry[9])
   );
   fullAdder fa9 (
       .a(in1[9]),
       .b(in2[9]),
-      .c(in3[9]),
+      .c(carry[9]),
       .sum(sum[9]),
-      .cout(in3[10])
+      .cout(carry[10])
   );
   fullAdder fa10 (
       .a(in1[10]),
       .b(in2[10]),
-      .c(in3[10]),
+      .c(carry[10]),
       .sum(sum[10]),
-      .cout(in3[11])
-  );
-  fullAdder fa11 (
-      .a(in1[11]),
-      .b(in2[11]),
-      .c(in3[11]),
-      .sum(sum[11]),
-      .cout(in3[12])
-  );
-  fullAdder fa12 (
-      .a(in1[12]),
-      .b(in2[12]),
-      .c(in3[12]),
-      .sum(sum[12]),
-      .cout(in3[13])
-  );
-  fullAdder fa13 (
-      .a(in1[13]),
-      .b(in2[13]),
-      .c(in3[13]),
-      .sum(sum[13]),
-      .cout(in3[14])
-  );
-  fullAdder fa14 (
-      .a(in1[14]),
-      .b(in2[14]),
-      .c(in3[14]),
-      .sum(sum[14]),
-      .cout(in3[15])
-  );
-  fullAdder fa15 (
-      .a(in1[15]),
-      .b(in2[15]),
-      .c(in3[15]),
-      .sum(sum[15]),
-      .cout(in3[16])
-  );
-  fullAdder fa16 (
-      .a(in1[16]),
-      .b(in2[16]),
-      .c(in3[16]),
-      .sum(sum[16]),
-      .cout(in3[17])
+      .cout(carry[11])
   );
 endmodule
 
